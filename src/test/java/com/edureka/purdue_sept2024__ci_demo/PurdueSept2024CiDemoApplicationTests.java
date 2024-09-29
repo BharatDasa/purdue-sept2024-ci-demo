227b5384f2376e3008d2909f72fa41b19a9d9cc7
Ramesh A <ramesharpu@gmail.com>
2024-09-29 11:04:43 +0530

    added unit test cases

--- src/test/java/com/edureka/purdue_sept2024__ci_demo/PurdueSept2024CiDemoApplicationTests.java
+++ src/test/java/com/edureka/purdue_sept2024__ci_demo/PurdueSept2024CiDemoApplicationTests.java
@@ -9,5 +9,19 @@ class PurdueSept2024CiDemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+	
+	@Test
+	public void m1() {
+		System.out.println("from m1");
+	}
+	
+	@Test
+	public void m2() {
+		System.out.println("from m2");
+	}
+	
+	@Test
+	public void m3() {
+		System.out.println("from m3");
+	}
 }
